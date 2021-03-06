006be4c5202f17020db5dda3442024a1ac5c5f11
cagataybalikci <cagataybalikci@gmail.com>
2021-03-07 01:42:59 +0300

    Tail Detection added.

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 
+# Global variables
 score = 0
+game_is_on = True
 
+# Screen configs
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -25,27 +28,28 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-# Game
-
-game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-    #Detect collision with Food.
+    # Detect collision with Food.
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+        snake.extend()
 
-    #Detect collision with Walls.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    # Detect collision with Walls.
+
+    if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
         game_is_on = False
         scoreboard.game_over()
 
-
-
-
+    # Tail detection.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
